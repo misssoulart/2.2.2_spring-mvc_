4af520f62145053edde1ea434371e7a5eca6eb95
Ekaterina Yakovenko <katya-sitnova@yandex.ru>
2022-09-14 11:47:41 +0300

    14.09.22_11.47time

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -24,7 +28,11 @@ public class CarsController {
     // Spring Framework. Урок 20: Модель. Передача данных от контроллера к представлению. (Алишев)
     @GetMapping()
     public String getSomeCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars", carService.getSomeCars(count));
-        return "car/cars";
+        System.out.println(count);
+        List<Car> carList = new ArrayList<>();
+        carList = carService.getSomeCars(count);
+        carList.stream().forEach(System.out::println);
+        //model.addAttribute("cars", carService.getSomeCars(count));
+        return "cars";
     }
 }
